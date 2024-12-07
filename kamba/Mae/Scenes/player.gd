bdaffa915fe71160f5c05cda91048407775aea66
katfrain <100097599+katfrain@users.noreply.github.com>
2024-12-07 17:11:46 -0500

    Move fix

--- kamba/Mae/Scenes/player.gd
+++ kamba/Mae/Scenes/player.gd
@@ -3,6 +3,10 @@ extends CharacterBody2D
 @onready var Being_sm = $BeingStateMachine
 @onready var Doing_sm = $DoingStateMachine
 var gravity: int = ProjectSettings.get_setting("physics/2d/default_gravity")
+var can_move: bool = true
+
+signal game_over
+signal win_level
 
 func _ready():
 	pass
@@ -16,3 +20,12 @@ func _process(delta: float) -> void:
 	Being_sm.process(delta)
 	Doing_sm.process(delta)
 	
+	
+func fall_death(body: Node2D) -> void:
+	game_over.emit()
+	can_move = false
+
+# if collision shape enters winzone, check if its the player, if it is, you win
+func win():
+	win_level.emit()
+	can_move = false
